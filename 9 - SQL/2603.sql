cbcf391743048deac9431af2c0c5202a18d13c4c
Andre Matte <carlosandrematte@gmail.com>
2021-07-10 09:55:34 -0300

    2603 and 2604 Done

---
+++ 9 - SQL/2603.sql
@@ -0,0 +1,5 @@
+-- URI Online Judge 2603
+
+SELECT name, street
+FROM customers
+WHERE city = 'Porto Alegre'
---
+++ 9 - SQL/2604.sql
@@ -0,0 +1,6 @@
+-- URI Online Judge 2604
+
+SELECT id, name
+FROM products
+WHERE  price < 10
+    OR price > 100 
