704a9910a87b9c34d6efbff2bc5af887e1b69b7d
Andre Mattos <carlosandrematte@gmail.com>
2021-07-22 11:55:38 -0300

    2747 Done

---
+++ 1 - Beginner/2747.py
@@ -0,0 +1,12 @@
+# URI Online Judge 2747
+
+linha = '---------------------------------------'
+espaco = '|                                     |'
+
+print(linha)
+print(espaco)
+print(espaco)
+print(espaco)
+print(espaco)
+print(espaco)
+print(linha)
